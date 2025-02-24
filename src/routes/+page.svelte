7bee7c28c7e7f68485a13cdb8767bb29d8e8caac
DroPlay20 <101497138+DroPlay20@users.noreply.github.com>
2025-02-24 18:02:42 +0200

    moved from /EuroMath2025 to /

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
 	import { onMount } from "svelte";
-	import { encript, decript } from "$lib/main";
-	//encript("qwertyuiop", "a1f4c5b3d9e0872fb4c1a2d3e5f60789");
+	import { decript } from "$lib/main";
 	onMount(() => {
 
 		setTimeout(() => {
@@ -48,7 +47,169 @@
 					<p
 						class="text-white text-[16px] font-normal font-['Montserrat'] break-words"
 					>
-						I think you are on the wrong page
+						Here is the messege you have to decrypt
+					</p>
+				</div>
+			</div>
+		</div>
+		<div id="msg2" class=" justify-start items-end gap-2.5 mb-5">
+			<img src="/bobpfp.svg" class="h-full" alt="" />
+			<div>
+				<div
+					class="h-[17px] pl-2.5 justify-start items-center gap-[15px] inline-flex"
+				>
+					<p
+						class="text-[#243c4a] text-[15px] font-normal font-['Montserrat']"
+					>
+						Bob
+					</p>
+				</div>
+				<div
+					class="px-5 py-2.5 bg-[#243c4a] overflow-hidden h-auto rounded-[15px]"
+				>
+					<p
+						id="textmsg2"
+						class="text-white text-[16px] font-normal font-['Montserrat'] break-all"
+					>
+					0b0801b9aded4864df220cfa157415089ea02b3499989956d536555cf1d5455a
+					</p>
+				</div>
+			</div>
+		</div>
+		<div id="msg3" class="justify-start items-end gap-2.5 mb-5">
+			<img src="/alicepfp.svg" class="h-full" alt="" />
+			<div>
+				<div
+					class="h-[17px] pl-2.5 justify-start items-center gap-[15px] inline-flex"
+				>
+					<p
+						class="text-[#243c4a] text-[15px] font-normal font-['Montserrat']"
+					>
+						Alice
+					</p>
+				</div>
+				<div
+					class="px-5 py-2.5 bg-[#243c4a] overflow-hidden h-auto rounded-[15px]"
+				>
+					<p
+						class="text-white text-[16px] font-normal font-['Montserrat'] break-words"
+					>
+						Now, send the key, that already typed for you
+					</p>
+				</div>
+			</div>
+		</div>
+		<div id="msg4" class="flex justify-start items-end gap-2.5 mb-5">
+			<div>
+				<div
+					class="h-[17px] w-full pr-2.5 justify-end items-center gap-[15px] inline-flex"
+				>
+					<p
+						class="text-[#243c4a] text-[15px] font-normal font-['Montserrat']"
+					>
+						You
+					</p>
+				</div>
+				<div
+					class="px-5 py-2.5 bg-[#243c4a] overflow-hidden h-auto rounded-[15px]"
+				>
+					<p
+						id="textmsg4"
+						class="text-white text-[16px] font-normal font-['Montserrat'] break-all"
+					></p>
+				</div>
+			</div>
+			<img src="/youpfp.svg" class="h-full" alt="" />
+		</div>
+		<div id="msg5" class=" justify-start items-end gap-2.5 mb-5">
+			<img src="/bobpfp.svg" class="h-full" alt="" />
+			<div>
+				<div
+					class="h-[17px] pl-2.5 justify-start items-center gap-[15px] inline-flex"
+				>
+					<p
+						class="text-[#243c4a] text-[15px] font-normal font-['Montserrat']"
+					>
+						Bob
+					</p>
+				</div>
+				<div
+					class="px-5 py-2.5 bg-[#243c4a] overflow-hidden h-auto rounded-[15px]"
+				>
+					<p
+						id="textmsg5"
+						class="text-white text-[16px] font-normal font-['Montserrat'] break-words"
+					>
+						
+					</p>
+				</div>
+			</div>
+		</div>
+		<div id="msg6" class="justify-start items-end gap-2.5 mb-5">
+			<img src="/alicepfp.svg" class="h-full" alt="" />
+			<div>
+				<div
+					class="h-[17px] pl-2.5 justify-start items-center gap-[15px] inline-flex"
+				>
+					<p
+						class="text-[#243c4a] text-[15px] font-normal font-['Montserrat']"
+					>
+						Alice
+					</p>
+				</div>
+				<div
+					class="px-5 py-2.5 bg-[#243c4a] overflow-hidden h-auto rounded-[15px]"
+				>
+					<p
+						class="text-white text-[16px] font-normal font-['Montserrat'] break-words"
+					>
+						Now, copy the key, and change just a few characters in
+						it, to see how it affects the message
+					</p>
+				</div>
+			</div>
+		</div>
+		<div id="msg7" class="flex justify-start items-end gap-2.5 mb-5">
+			<div>
+				<div
+					class="h-[17px] w-full pr-2.5 justify-end items-center gap-[15px] inline-flex"
+				>
+					<p
+						class="text-[#243c4a] text-[15px] font-normal font-['Montserrat']"
+					>
+						You
+					</p>
+				</div>
+				<div
+					class="px-5 py-2.5 bg-[#243c4a] overflow-hidden h-auto rounded-[15px]"
+				>
+					<p
+						id="textmsg7"
+						class="text-white text-[16px] font-normal font-['Montserrat'] break-all"
+					></p>
+				</div>
+			</div>
+			<img src="/youpfp.svg" class="h-full" alt="" />
+		</div>
+		<div id="msg8" class=" justify-start items-end gap-2.5 mb-5">
+			<img src="/bobpfp.svg" class="h-full" alt="" />
+			<div>
+				<div
+					class="h-[17px] pl-2.5 justify-start items-center gap-[15px] inline-flex"
+				>
+					<p
+						class="text-[#243c4a] text-[15px] font-normal font-['Montserrat']"
+					>
+						Bob
+					</p>
+				</div>
+				<div
+					class="px-5 py-2.5 bg-[#243c4a] overflow-hidden h-auto rounded-[15px]"
+				>
+					<p
+						id="textmsg8"
+						class="text-white text-[16px] font-normal font-['Montserrat'] break-all"
+					>
 					</p>
 				</div>
 			</div>
@@ -60,12 +221,82 @@
 		<input
 			id="messege"
 			class="text-[#6e6e6e] bg-transparent border-none text-base font-bold font-['Montserrat']"
+			value="a1f4c5b3d9e0872fb4c1a2d3e5f60789"
 			placeholder="Message"
+			onkeydown={async (e) => {
+				if (e.key !== 'Enter') return;
+				const button = document.querySelector("button");
+				if(button)
+					button.click();
+			}}
 		/>
 
 		<button
-
 			class="px-[18px] py-2 bg-[#243c4a] rounded-[14px] justify-center items-center gap-2.5 flex overflow-hidden"
+			onkeydown={async (e) => {
+				if (e.key !== 'Enter') return;
+				const button = document.querySelector("button");
+				if(button)
+					button.click();
+			}}
+			onclick={() => {
+				const msgcontainer = document.getElementById("msgcontainer");
+				const msg = document.getElementById(
+					"messege",
+				) as HTMLInputElement;
+				const textmsg2 = document.getElementById("textmsg2");
+				const textmsg4 = document.getElementById("textmsg4");
+				const textmsg7 = document.getElementById("textmsg7");
+				const msg4 = document.getElementById("msg4");
+				const msg7 = document.getElementById("msg7");
+				if (msg && msgcontainer && textmsg4 && msg4 && msg7 && textmsg7 && textmsg2) {
+					if (msg.value.length !== 32) {
+						alert("The message must be 32 characters long.");
+						return;
+					}
+					if (msg4.style.display != "flex") {
+						const key = msg.value
+						msg.value = "";
+						textmsg4.innerText = key;
+						msg4.style.display = "flex";
+						msgcontainer.scrollTop = msgcontainer.scrollHeight;
+						setTimeout(() => {
+							const msg5 = document.getElementById("msg5");
+							const textmsg5 = document.getElementById("textmsg5");
+							if (msg5 && textmsg5) {
+								textmsg5.innerText = "Your message is: "+decript(textmsg2.innerText, key);
+								msg5.style.display = "flex";
+								msgcontainer.scrollTop =
+									msgcontainer.scrollHeight;
+							}
+						}, 1500);
+						setTimeout(() => {
+							const msg6 = document.getElementById("msg6");
+							if (msg6) {
+								msg6.style.display = "flex";
+								msgcontainer.scrollTop =
+									msgcontainer.scrollHeight;
+							}
+						}, 4000);
+					} else {
+						const key = msg.value
+						msg.value = "";
+						textmsg7.innerText = key;
+						msg7.style.display = "flex";
+						msgcontainer.scrollTop = msgcontainer.scrollHeight;
+						setTimeout(() => {
+							const msg8 = document.getElementById("msg8");
+							const textmsg8 = document.getElementById("textmsg8");
+							if (msg8 && textmsg8) {
+								textmsg8.innerText = "Your message is: "+decript(textmsg2.innerText, key);
+								msg8.style.display = "flex";
+								msgcontainer.scrollTop =
+									msgcontainer.scrollHeight;
+							}
+						}, 1500);
+					}
+				}
+			}}
 		>
 			<p
 				class="text-[#f2f2f2] text-[13px] font-semibold font-['Montserrat']"
@@ -75,7 +306,10 @@
 		</button>
 	</div>
 </div>
-
+<div class="flex items-center justify-center opacity-40">
+	<img src="/info.svg" class="w-4" alt="" />
+	<p>Key: a1f4c5b3d9e0872fb4c1a2d3e5f60789</p>
+</div>
 
 <style>
 	@keyframes fadeIn {
@@ -92,5 +326,32 @@
 	#msg1 {
 		animation: fadeIn 1s ease-out;
 	}
-
+	#msg2 {
+		display: none;
+		animation: fadeIn 1s ease-out;
+	}
+	#msg3 {
+		display: none;
+		animation: fadeIn 1s ease-out;
+	}
+	#msg4 {
+		display: none;
+		animation: fadeIn 1s ease-out;
+	}
+	#msg5 {
+		display: none;
+		animation: fadeIn 1s ease-out;
+	}
+	#msg6 {
+		display: none;
+		animation: fadeIn 1s ease-out;
+	}
+	#msg7 {
+		display: none;
+		animation: fadeIn 1s ease-out;
+	}
+	#msg8 {
+		display: none;
+		animation: fadeIn 1s ease-out;
+	}
 </style>
