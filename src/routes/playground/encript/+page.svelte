54b4b763f24144f91c9dd84774c0316b5635e143
DroPlay20 <101497138+DroPlay20@users.noreply.github.com>
2025-06-09 04:20:08 +0300

    added playground

---
+++ src/routes/playground/encript/+page.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { encript } from "$lib/main";
+</script>
+<div>
+<input type="text" id="key" placeholder="key">
+<input type="text" id="text" placeholder="text">
+<button onclick={() => {
+	const textInput = document.getElementById("text") as HTMLInputElement;
+	const keyInput = document.getElementById("key") as HTMLInputElement;
+	const resultDiv = document.getElementById("result") as HTMLDivElement;
+	if(textInput && keyInput && resultDiv) {
+		const encriptedText = encript(textInput.value, keyInput.value);
+		if(encriptedText)
+			resultDiv.innerText = encriptedText;
+		else 
+			resultDiv.innerText = "Error :("
+	}
+	
+	}
+}>Encript</button>
+<div id="result"></div>
+</div>
