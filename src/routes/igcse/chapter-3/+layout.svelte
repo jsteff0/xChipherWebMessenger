7623cabc4631c9ba6641fe7b176ab5896d5ccf8b
DroPlay20 <101497138+DroPlay20@users.noreply.github.com>
2026-03-18 15:50:15 +0200

    Stop tracking chapters 2-6

--- src/routes/igcse/chapter-3/+layout.svelte
+++
@@ -1,9 +0,0 @@
-<script lang="ts">
-	import '../../app.css';
-	import { dev } from '$app/environment';
-	import { injectAnalytics } from '@vercel/analytics/sveltekit';
-	injectAnalytics({ mode: dev ? 'development' : 'production' });
-	let { children } = $props();
-</script>
-
-{@render children()}
